42ca74f4b393bc7351867b469f9629eddbc1952d
santiloopz <santilpzate@gmail.com>
2021-02-11 08:43:55 +0100

    [Trigonometrius] created printMenu func

--- utils/trigonometry/trigonometrius/src/trigonometrius.c
+++ utils/trigonometry/trigonometrius/src/trigonometrius.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int main() {
+void printMenu() {
 	printf("( T | r | i | g | o | n | o | m | e | t | r | i | u | s )\n\n");
 	printf("Select what elements you know:\n");
 	printf("0 -> angle in deg\n");
 	printf("1 -> angle in rad\n");
 	printf("2 -> oppesed side\n");
 	printf("3 -> adjacent side\n");
+};
+
+int main() {
+	printMenu();
 
 	int number;
 	scanf("%d", &number);
