f65f45d99152c640c6881389f690cc26740e07ab
santiloopz <santilpzate@gmail.com>
2021-02-12 08:12:14 +0100

    configuring trigonometrius

--- utils/trigonometry/trigonometrius/src/trigonometrius.c
+++ utils/trigonometry/trigonometrius/src/trigonometrius.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
 void printMenu() {
-	printf("( T | r | i | g | o | n | o | m | e | t | r | i | u | s )\n\n");
 	printf("Select what elements you know:\n");
-	printf("0 -> angle in deg\n");
-	printf("1 -> angle in rad\n");
-	printf("2 -> oppesed side\n");
-	printf("3 -> adjacent side\n");
+	printf("0 -> angle\n");
+	printf("1 -> oppesed side\n");
+	printf("2 -> adjacent side\n");
 };
 
 int main() {
-	printMenu();
+	printf("( T | r | i | g | o | n | o | m | e | t | r | i | u | s )\n\n");
 
-	int number;
-	scanf("%d", &number);
+	while(1) {
+		printMenu();
 
-	if (number > 0 && number < 4) {
-		printf("Success!!!\n");
-	}
+		int number;
+		scanf("%d", &number);
+
+		if (number == 0) {
+			printf("Choose angle measure type\n");
+		}
+
+		if (number > 0 && number < 4) {
+			printf("Success!!!\n");
+		};
+	};
 }
